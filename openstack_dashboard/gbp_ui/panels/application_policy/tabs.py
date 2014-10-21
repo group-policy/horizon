afb50aaba44452baddeed6f02029b0719b61db2d
uday bhaskar <gotouday@gmail.com>
2014-10-22 00:39:01 +0530

    fixed policy rule set details view

--- openstack_dashboard/gbp_ui/panels/application_policy/tabs.py
+++ openstack_dashboard/gbp_ui/panels/application_policy/tabs.py
@@ -143,7 +143,10 @@ class ContractDetailsTab(tabs.Tab):
                 action_list = []
                 for aid in rule.policy_actions:
                     action = client.policyaction_get(request,aid)
-                    action_list.append(action.name+":"+str(action.id)+":"+str(action.action_type))
+                    if action.action_value:
+                        action_list.append(str(action.action_type)+":"+str(action.action_value))
+                    else:
+                        action_list.append(str(action.action_type))
                 r['actions'] = action_list
                 r['classifier'] = client.policyclassifier_get(request,rule.policy_classifier_id)
                 rules_with_details.append(r)
